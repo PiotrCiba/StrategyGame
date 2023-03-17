425b24d1d448deec3df3e9cfa1583da60b5d8c13
PiotrCiba <piotr.m.ciba@gmail.com>
2023-03-17 19:44:05 +0100

    First attempt at a Main Menu (doesn't work yet ;p) #TODO: Game states #TODO: In-game features #TODO: Actual working Main Menu, launching the game

--- StrategyGame/Views/MainWindow.axaml.cs
+++ StrategyGame/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace StrategyGame.Views;
 
@@ -8,4 +9,8 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+    public void NewGameButton_Click(object? sender, RoutedEventArgs routedEventArgs) { }
+    public void LoadGameButton_Click(object? sender, RoutedEventArgs routedEventArgs) { }
+    public void OptionsButton_Click(object? sender, RoutedEventArgs routedEventArgs){}
+    public void ExitButton_Click(object? sender, RoutedEventArgs routedEventArgs){}
 }
