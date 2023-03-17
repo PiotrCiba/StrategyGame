7d34f65f8b8cb2bf44c3f1dff32d2aa65fc80352
PiotrCiba <piotr.m.ciba@gmail.com>
2023-03-17 17:40:55 +0100

    first commit

---
+++ StrategyGame/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace StrategyGame.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ StrategyGame/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace StrategyGame.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
